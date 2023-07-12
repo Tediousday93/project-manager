4532b64a46255732421f2fe2ef5022144b11777a
ROWAN <kimkj93@naver.com>
2023-07-12 19:02:12 +0900

    feat: MainViewController / MainViewModel 정의

---
+++ ProjectManager/ProjectManager/MainView/MainViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MainViewModel.swift
+//  ProjectManager
+//
+//  Created by Rowan on 2023/07/12.
+//
+
+import Foundation
+
+final class MainViewModel {
+    private var projectList: [Project] = []
+}
