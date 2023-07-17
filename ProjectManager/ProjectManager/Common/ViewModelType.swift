1bce344a071ee6506aa6cfe412fecab146c3fe7c
ROWAN <kimkj93@naver.com>
2023-07-17 15:34:58 +0900

    feat: ViewModelType Protocol 정의

---
+++ ProjectManager/ProjectManager/Common/ViewModelType.swift
@@ -0,0 +1,13 @@
+//
+//  ViewModelType.swift
+//  ProjectManager
+//
+//  Created by Rowan on 2023/07/13.
+//
+
+protocol ViewModelType: AnyObject {
+    associatedtype Input
+    associatedtype Output
+    
+    func transform(input: Input) -> Output
+}
