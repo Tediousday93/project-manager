a9282ced6544c0e1d52f1948428ca6972b3a9d01
ROWAN <kimkj93@naver.com>
2023-09-12 21:50:42 +0900

    refactor: CDProject 타입 conversion protocol 채택 및 프로퍼티 타입 수정
    Persistable, DomainConvertible 채택
    프로퍼티 타입 non-optional로 변경

--- ProjectManager/ProjectManager/Data/CoreData/Entity/CDProject+CoreDataProperties.swift
+++ ProjectManager/ProjectManager/Data/CoreData/Entity/CDProject+CoreDataProperties.swift
@@ -16,16 +16,38 @@ extension CDProject {
         return NSFetchRequest<CDProject>(entityName: "CDProject")
     }
 
-    @NSManaged public var title: String?
-    @NSManaged public var date: Date?
-    @NSManaged public var body: String?
-    @NSManaged public var state: String?
-    @NSManaged public var id: UUID?
+    @NSManaged public var title: String
+    @NSManaged public var date: Date
+    @NSManaged public var body: String
+    @NSManaged public var state: String
+    @NSManaged public var id: UUID
 
 }
 
 extension CDProject: Identifiable { }
 
-extension CDProject: CoreDataRepresentable {
-    typealias Domain = Project
+extension CDProject: Persistable { }
+
+extension CDProject: DomainConvertible {
+    typealias DomainType = Project
+    
+    func toDomain() -> Project {
+        return Project(title: title,
+                       date: date,
+                       body: body,
+                       state: Project.State(rawValue: state) ?? .todo,
+                       id: id)
+    }
+}
+
+extension Project: CoreDataRepresentable {
+    typealias CoreDataType = CDProject
+    
+    func update(entity: CDProject) {
+        entity.title = title
+        entity.date = date
+        entity.body = body
+        entity.state = state.rawValue
+        entity.id = id
+    }
 }
